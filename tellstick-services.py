50cd0d43210f46fbd8f4fe814efa1363040b928d
Andreas Svanberg <ante.svanberg@gmail.com>
2012-11-05 22:54:21 +0100

    added options parser

--- tellstick-services.py
+++ tellstick-services.py
@@ -1,8 +1,12 @@
 from flask import Flask,jsonify
 import simplejson 
 import json
+import optparse
 
+
+TSS_NAME="tellstick-services"
 app = Flask(__name__)
+
 
 methods=['GET']
 @app.route("/")
@@ -10,17 +14,20 @@ def start():
 	"""Just the index request"""
 	return jsonify(status='running',status_url='/status')
 
+
 methods=['GET']
 @app.route("/status")
 def status():
 	"""On status request return the list of all devices and their status"""
 	return "status"
 
+
 methods=['GET']
 @app.route("/status-update")
 def update_status():
 	"""On status update request reread state from telldusd and the return status"""
 	return "will update status"
+
 
 methods=['GET']
 @app.route("/device/<int:device_id>")
@@ -29,6 +36,7 @@ def device_status(device_id):
 	app.logger.debug('checking logger')
 	return jsonify(device_id=device_id, status='on')
 
+
 methods=['POST']
 @app.route("/device/<int:device_id>/<command>")
 def controll_device(device_id, command):
@@ -36,5 +44,23 @@ def controll_device(device_id, command):
 	return "updating device %s with command %s" % device_id, command
 
 
+def init_opts():
+	usage = "Support the following arguments"
+	
+	parser = optparse.OptionParser(usage = usage)
+	parser.add_option("-d", "--debug", 
+						action="store_true", dest="debug", 
+						default=False,help="Run in debug mode")
+	return parser.parse_args()
+
+
+def handle_opts():
+	(options, args) =init_opts()
+	if options.debug:
+		print "starting in debug mode"
+		app.debug = True
+
+
 if __name__ == "__main__":
-    app.run()
+	handle_opts()
+	app.run()
