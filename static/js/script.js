6765598cdccf69067d8a05c9b7071946e2fe190a
Andreas Svanberg <andreas.svanberg@dewire.com>
2012-11-08 15:07:24 +0100

    get device from service

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    alert('apa');
+
+    console.log('getting units from telldus')
+
+    $.get('/status', function(data) {
+
+        console.log('Got data: ' + data);
+
+        devices = JSON.parse(data);
+
+        $.each(devices, function(i){
+            console.log(devices[i]);
+            var deviceDiv = $('<div />');
+            $(deviceDiv).append('<h2>' + devices[i].deviceId + '-' + devices[i].deviceName + '</h2>');
+            $(deviceDiv).append('<p>Status: ' + devices[i].status + '</p>');
+            $('#content-wrapper').append(deviceDiv);
+
+        });
+
+    });
+
+
 });
